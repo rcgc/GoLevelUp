efd607f2aec856c64797acec15894e666cc5c119
rcgc <roberto140298@gmail.com>
2022-12-30 15:18:42 -0600

    concurrency problems finished

---
+++ Concurrency/Level1/Copying.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+/*
+* Given a list of messages and a number N
+* implement a function that outputs the
+* same  message N times concurrently.
+ */
+
+var messages = []string{
+	"Hello!",
+	"How are you?",
+	"Are you just going to repeat what I say?",
+	"So immature",
+	"Stop copying me!",
+}
+
+// repeat concurrently prints out the given message n times
+func repeat(n int, message string){
+	ch := make(chan struct{})
+	for i := 0; i < n; i++ {
+		go func(i int){
+			log.Printf("[G%d]:%s\n", i, message)
+			ch <- struct{}{}
+		}(i)
+	}
+	for i := 0; i < n; i++{
+		<-ch
+	}
+}
+
+func main(){
+	factor := flag.Int64("factor", 0, "The fan-out factor to repeat by")
+	flag.Parse()
+	for _, m := range messages {
+		log.Println(m)
+		repeat(int(*factor), m)
+	}
+}
---
+++ Concurrency/Level3/Lunch.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+/*
+* Given a defined list of resources
+* implement a function that simulates the
+* concurrent allocation of resources to
+* consumer goroutines
+ */
+
+// the number of attendees we need to serve lunch to
+const consumerCount = 300
+
+// foodCourses represents the types of resources to pass to the consumers
+var foodCourses = []string{
+	"Caprese Salad",
+	"Spaghetti Carbonara",
+	"Vanilla Panna Cotta",
+}
+
+// takeLunch is the consumer function for the lunch simulation
+// Change the signature of this function as required
+func takeLunch(name string, in []chan string, done chan<- struct{}) {
+	for _, ch := range in {
+		log.Printf("%s eats %s.\n", name, <-ch)
+	}
+	done <- struct{}{}
+}
+
+// serveLunch is the producer function for the lunch simulation.
+// Change the signature of this function as required
+func serveLunch(course string, out chan<- string, done <- chan struct{}) {
+	for {
+		select {
+		case out <- course:
+		case <- done:
+			return
+		}
+	}
+}
+
+func main() {
+	log.Printf("Welcome to the conference lunch! Serving %d attendees.\n",
+		consumerCount)
+	var courses []chan string
+	doneEating := make(chan struct{})
+	doneServing := make(chan struct{})
+
+	for _, c := range foodCourses{
+		ch := make(chan string)
+		courses = append(courses, ch)
+		go serveLunch(c, ch, doneServing)
+	}
+
+	for i := 0; i < consumerCount; i++{
+		name := fmt.Sprintf("Attendee %d", i)
+		go takeLunch(name, courses, doneEating)
+	}
+
+	for i := 0; i < consumerCount; i++{
+		<-doneEating
+	}
+
+	close(doneServing)
+}
